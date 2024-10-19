6ac13d42fc0dfde019f10b4af5b779c5ea7f83f8
vladimir <vladimir@osipov.cc>
2024-10-19 19:16:38 +0100

    init_2

--- scrape_contacts.py
+++ scrape_contacts.py
@@ -6,67 +6,61 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
 from googlesearch import search
-logging.getLogger('scrapy').propagate = False
-
-def get_urls(tag, n, language):
-    urls = [url for url in search(tag, stop=n, lang=language)][:n]
-    return urls
-
-print(get_urls('musical instruments companies', 5 , 'en'))
-
-class MailSpider(scrapy.Spider):
-    name = 'email'
-
-    def parse(self, response):
-
-        links = LxmlLinkExtractor(allow=()).extract_links(response)
-        links = [str(link.url) for link in links]
-        links.append(str(response.url))
-
-        for link in links:
-            yield scrapy.Request(url=link, callback=self.parse_link)
-
-    def parse_link(self, response):
-
-        for word in self.reject:
-            if word in str(response.url):
-                return
-
-        html_text = str(response.text)
-        mail_list = re.findall('\w+@\w+\.{1}\w+', html_text)
-
-        dic = {'email': mail_list, 'link': str(response.url)}
-        df = pd.DataFrame(dic)
-
-        df.to_csv(self.path, mode='a', header=False)
-        df.to_csv(self.path, mode='a', header=False)
 
 
+def scrape_emails(tag, n, language, path, reject=[]):
+    logging.getLogger('scrapy').propagate = False
 
-def ask_user(question):
-    response = input(question + ' y/n' + '\n')
-    if response == 'y':
+    def get_urls(tag, n, language):
+        urls = [url for url in search(tag, stop=n, lang=language)][:n]
+        return urls
+
+    class MailSpider(scrapy.Spider):
+        name = 'email_spider'
+
+        def __init__(self, start_urls, path, reject):
+            self.start_urls = start_urls
+            self.path = path
+            self.reject = reject
+            super().__init__()
+
+        def parse(self, response):
+            links = LxmlLinkExtractor(allow=()).extract_links(response)
+            links = [str(link.url) for link in links]
+            links.append(str(response.url))
+
+            for link in links:
+                yield scrapy.Request(url=link, callback=self.parse_link)
+
+        def parse_link(self, response):
+            for word in self.reject:
+                if word in str(response.url):
+                    return
+
+            html_text = str(response.text)
+            mail_list = re.findall(r'\w+@\w+\.\w+', html_text)
+
+            if mail_list:
+                dic = {'email': mail_list, 'link': [response.url] * len(mail_list)}
+                df = pd.DataFrame(dic)
+                df.to_csv(self.path, mode='a', header=False, index=False)
+
+    def create_file(path):
+        if os.path.exists(path):
+            response = input(f"File '{path}' already exists. Replace? (y/n): ")
+            if response.lower() != 'y':
+                print("Operation cancelled.")
+                return False
+            else:
+                os.remove(path)
+        df = pd.DataFrame(columns=['email', 'link'])
+        df.to_csv(path, mode='w', header=True, index=False)
         return True
-    else:
-        return False
 
+    if not create_file(path):
+        return
 
-def create_file(path):
-    response = False
-    if os.path.exists(path):
-        response = ask_user('File already exists, replace?')
-        if response == False: return
-
-    with open(path, 'wb') as file:
-        file.close()
-
-
-def get_info(tag, n, language, path, reject=[]):
-    create_file(path)
-    df = pd.DataFrame(columns=['email', 'link'], index=[0])
-    df.to_csv(path, mode='w', header=True)
-
-    print('Collecting Google urls...')
+    print('Collecting Google URLs...')
     google_urls = get_urls(tag, n, language)
 
     print('Searching for emails...')
@@ -75,14 +69,16 @@ def get_info(tag, n, language, path, reject=[]):
     process.start()
 
     print('Cleaning emails...')
-    df = pd.read_csv(path, index_col=0)
-    df.columns = ['email', 'link']
+    df = pd.read_csv(path)
     df = df.drop_duplicates(subset='email')
     df = df.reset_index(drop=True)
-    df.to_csv(path, mode='w', header=True)
+    df = df.drop_duplicates(subset='email', keep='first')
+    df.to_csv(path, mode='w', header=True, index=False)
 
+    print(f"Emails saved to '{path}'")
     return df
 
+# Example usage:
 bad_words = ['facebook', 'instagram', 'youtube', 'twitter', 'wiki']
-df = get_info('mastering studio london', 300, 'pt', 'results.csv', reject=bad_words)
+df = scrape_emails('crm companies in london', 20, 'en', 'results.csv', reject=bad_words)
 print(df.head())
